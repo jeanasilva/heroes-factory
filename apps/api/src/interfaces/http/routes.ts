efa0f771a0d2921bb6b7c98c356dbd7aa25a812b
jean.silva <jeansoftware67@gmail.com>
2026-06-22 16:32:19 -0300

    feat: expose swagger documentation routes

--- apps/api/src/interfaces/http/routes.ts
+++ apps/api/src/interfaces/http/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { PrismaHeroesRepository } from '../../infra/database/prisma/repositories/prisma-heroes-repository.js';
+import { createDocsRoutes } from './docs/docs-routes.js';
 import { createHeroesRoutes } from './heroes-routes.js';
 
 const routes = Router();
@@ -8,6 +9,7 @@ routes.get('/health', (_request, response) => {
   response.json({ status: 'ok' });
 });
 
+routes.use(createDocsRoutes());
 routes.use(createHeroesRoutes(new PrismaHeroesRepository()));
 
 export { routes };
